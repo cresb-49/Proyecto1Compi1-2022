87d19729d81e0d4ec8ec4a3d1517e8d46e0fbc44
benjamin <carlosbpac@gmail.com>
2022-03-11 21:12:37 -0600

    eliminado de standalone

--- AppCliente/src/main/java/com/cresb49/appcliente/analizadores/def/LexerDef.java
+++ AppCliente/src/main/java/com/cresb49/appcliente/analizadores/def/LexerDef.java
@@ -1399,55 +1399,5 @@ public class LexerDef implements java_cup.runtime.Scanner {
     }
   }
 
-  /**
-   * Runs the scanner on input files.
-   *
-   * This is a standalone scanner, it will print any unmatched
-   * text to System.out unchanged.
-   *
-   * @param argv   the command line, contains the filenames to run
-   *               the scanner on.
-   */
-  public static void main(String[] argv) {
-    if (argv.length == 0) {
-      System.out.println("Usage : java LexerDef [ --encoding <name> ] <inputfile(s)>");
-    }
-    else {
-      int firstFilePos = 0;
-      String encodingName = "UTF-8";
-      if (argv[0].equals("--encoding")) {
-        firstFilePos = 2;
-        encodingName = argv[1];
-        try {
-          // Side-effect: is encodingName valid?
-          java.nio.charset.Charset.forName(encodingName);
-        } catch (Exception e) {
-          System.out.println("Invalid encoding '" + encodingName + "'");
-          return;
-        }
-      }
-      for (int i = firstFilePos; i < argv.length; i++) {
-        LexerDef scanner = null;
-        try {
-          java.io.FileInputStream stream = new java.io.FileInputStream(argv[i]);
-          java.io.Reader reader = new java.io.InputStreamReader(stream, encodingName);
-          scanner = new LexerDef(reader);
-          while ( !scanner.zzAtEOF ) scanner.next_token();
-        }
-        catch (java.io.FileNotFoundException e) {
-          System.out.println("File not found : \""+argv[i]+"\"");
-        }
-        catch (java.io.IOException e) {
-          System.out.println("IO error scanning file \""+argv[i]+"\"");
-          System.out.println(e);
-        }
-        catch (Exception e) {
-          System.out.println("Unexpected exception:");
-          e.printStackTrace();
-        }
-      }
-    }
-  }
-
 
 }
--- Jflex and Cup source/DEF/LexerDef.jflex
+++ Jflex and Cup source/DEF/LexerDef.jflex
@@ -8,7 +8,6 @@ import java_cup.runtime.*;
 
 %%
 %class LexerDef
-%standalone
 %unicode
 %public
 %line
