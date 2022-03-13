179adad59926b1b0befe1889358a859c2136e1d8
benjamin <carlosbpac@gmail.com>
2022-03-12 20:00:22 -0600

    Excepcion al no agregar un resultado json

---
+++ AppCliente/src/main/java/com/cresb49/appcliente/analizadores/def/obj/exceptions/NoReporteJson.java
@@ -0,0 +1,10 @@
+package com.cresb49.appcliente.analizadores.def.obj.exceptions;
+
+public class NoReporteJson extends Exception{
+    public NoReporteJson() {
+    }
+    public NoReporteJson(String message) {
+        super(message);
+    }
+    
+}
