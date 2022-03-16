cc417680dc88fd23b331e7989f4e4fb4fda54956
benjamin <carlosbpac@gmail.com>
2022-03-15 21:37:01 -0600

    ejecucion de la tabla

--- AppCliente/src/main/java/com/cresb49/appcliente/analizadores/def/obj/RenderizarHTML.java
+++ AppCliente/src/main/java/com/cresb49/appcliente/analizadores/def/obj/RenderizarHTML.java
@@ -11,6 +11,8 @@ public class RenderizarHTML {
     private Pila<Integer> pila_bucle;
     private Pila<String> var_bucle;
 
+    private boolean modo_ejecucion;
+
     public RenderizarHTML() {
     }
 
@@ -41,6 +43,7 @@ public class RenderizarHTML {
 
         Token temp_ejecucion;
         for (Integer instruccion = 0; instruccion < tablaEjecucion.getFilas().size(); instruccion++) {
+            System.out.println("Ejecutando la instruccion #"+instruccion);
             temp_ejecucion = tablaEjecucion.getFilas().get(instruccion);
 
             switch (temp_ejecucion.getAccion()) {
@@ -48,6 +51,13 @@ public class RenderizarHTML {
                     hmtl = hmtl + temp_ejecucion.getValorToken() + "\n";
                     break;
                 case Token.BUCLE_INI:
+                    int temp_instruccion = instruccion;
+                    pila_bucle.push(temp_instruccion);
+                    instruccion++;
+                    if(!this.validarCiclo(tablaEjecucion.getFilas().get(instruccion),tablaEjecucion.getFilas().get(instruccion+1))){
+                        this.aisg_modo_ejecucion(false);
+                    }
+                    instruccion++;
                     break;
                 case Token.BUCLE_FIN:
                     break;
@@ -62,5 +72,30 @@ public class RenderizarHTML {
         }
         return hmtl;
     }
+
+    private boolean validarCiclo(Token var, Token valMax) {
+        FilaTabla variable = tablaSimbolos.buscar(var.getLexema());
+        if(variable.getValor()==null){
+            System.out.println("La variable \""+variable.getNombre()+"\" es nula");
+        }else{
+            FilaTabla var_max = tablaSimbolos.buscar(valMax.getLexema());
+            if(var_max == null){
+                int valor_maximo = (int) valMax.getValorToken();
+                int val_variable = (int) variable.getValor();
+                return val_variable <= valor_maximo;
+            }else{
+                int valor_maximo = (int) var_max.getValor();
+                int val_variable = (int) variable.getValor();
+                return val_variable <= valor_maximo;
+            }
+        }
+        return false;
+    }
+
+    public void aisg_modo_ejecucion(boolean estado){
+        this.modo_ejecucion = estado;
+    }
+
+    
 
 }
