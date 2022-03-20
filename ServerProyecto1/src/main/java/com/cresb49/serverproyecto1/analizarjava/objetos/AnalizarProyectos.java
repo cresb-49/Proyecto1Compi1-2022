5e34edb8e2a829db0314d39881c77f8ee60960f5
benjamin <carlosbpac@gmail.com>
2022-03-19 19:43:27 -0600

    imprecion de los archivos recibidos

--- ServerProyecto1/src/main/java/com/cresb49/serverproyecto1/analizarjava/objetos/AnalizarProyectos.java
+++ ServerProyecto1/src/main/java/com/cresb49/serverproyecto1/analizarjava/objetos/AnalizarProyectos.java
@@ -11,10 +11,10 @@ public class AnalizarProyectos {
     public void realizarAnalisis(Proyecto proyecto){
 
         for (FileJava archivo : proyecto.getCarpeta1()) {
-            System.out.println("Archivo Proyecto 1 -> "+archivo.getName());
+            System.out.println(proyecto.getNombreCarpeta1()+" -> "+archivo.getName());
         }
         for (FileJava archivo : proyecto.getCarpeta2()) {
-            System.out.println("Archivo Proyecto 2 -> "+archivo.getName());
+            System.out.println(proyecto.getNombreCarpeta2()+" -> "+archivo.getName());
         }
     }
 }
