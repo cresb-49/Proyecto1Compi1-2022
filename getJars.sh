1f7bb0bbbfa5d606d3fd2b280a02788edb39fde7
benjamin <carlosbpac@gmail.com>
2022-03-23 00:54:01 -0600

    script para recuperar los jar's y capeta de almacenamiento de los mismos

---
+++ getJars.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+echo "MOVIENDO JAR APP_CLIENTE"
+mv AppCliente/target/APP_CLIENTE-jar-with-dependencies.jar JARs
+echo "MOVIENDO JAR SERVIDOR"
+mv ServerProyecto1/target/SERVIDOR-jar-with-dependencies.jar JARs
+
+
